e513c21b808c49c9500545c6687081821e390fd5
LXMAJS <lxmajs@163.com>
2018-10-11 11:22:39 +0800

    data source config compeleted

--- src/main/java/com/lxmajs/demo/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/lxmajs/demo/config/dao/DataSourceConfiguration.java
@@ -1,14 +1,28 @@
 package com.lxmajs.demo.config.dao;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.beans.PropertyVetoException;
 
+@Configuration
+// 配置mybatis mapper的扫描路径
+@MapperScan("com.lxmajs.demo.dao ")
 public class DataSourceConfiguration {
+
+    @Value("${jdbc.driver}")
     private String jdbcDriver;
+
+    @Value("${jdbc.url}")
     private String jdbcUrl;
+
+    @Value("${jdbc.username}")
     private String jdbcUserName;
+
+    @Value("${jdbc.password}")
     private String jdbcPassword;
 
     @Bean(name = "dataSource")
