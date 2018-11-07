00e1f454a1db011d58a1392ba1adb5624fd3e666
LXMAJS <lxmajs@163.com>
2018-11-07 17:29:52 +0800

    redis hash oper

--- src/main/java/com/lxmajs/demo/util/RedisOperator.java
+++ src/main/java/com/lxmajs/demo/util/RedisOperator.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -55,6 +56,12 @@ public class RedisOperator {
     }
 
     /**
+     * ------------
+     * 字符串
+     * ------------
+     */
+
+    /**
      * 删除某个键值对
      * @param key
      */
@@ -92,6 +99,12 @@ public class RedisOperator {
     }
 
     /**
+     * ------------
+     * 哈希表
+     * ------------
+     */
+
+    /**
      * 实现命令：HSET key field value，将哈希表 key中的域 field的值设为 value
      * @param key
      * @param field
@@ -100,5 +113,35 @@ public class RedisOperator {
     public void hset(String key, String field, Object value){
         redisTemplate.opsForHash().put(key, field, value);
     }
+
+    /**
+     * 实现命令：HGET key field，返回哈希表 key中给定域 field的值
+     * @param key
+     * @param field
+     * @return
+     */
+    public String hget(String key, String field){
+        return (String)redisTemplate.opsForHash().get(key, field);
+    }
+
+    /**
+     * HDEL key field [field ...]，删除哈希表 key 中的一个或多个指定域，不存在的域将被忽略。
+     * @param key
+     * @param field
+     */
+    public void hdel(String key, String field){
+        redisTemplate.opsForHash().delete(key, field);
+    }
+
+    /**
+     * 实现命令：HGETALL key，返回哈希表 key中，所有的域和值。
+     * @param key
+     * @return
+     */
+    public Map<Object, Object> hgetall(String key){
+        return redisTemplate.opsForHash().entries(key);
+    }
+
+    
 
 }
