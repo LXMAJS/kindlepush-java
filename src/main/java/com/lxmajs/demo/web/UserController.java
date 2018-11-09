31f80cbe811a3948ca922faf8dfeaa9fd39d5507
LXMAJS <lxmajs@163.com>
2018-11-09 17:45:28 +0800

    http client util compeleted

--- src/main/java/com/lxmajs/demo/web/UserController.java
+++ src/main/java/com/lxmajs/demo/web/UserController.java
@@ -1,5 +1,9 @@
 package com.lxmajs.demo.web;
 
+import com.lxmajs.demo.model.WXSessionModel;
+import com.lxmajs.demo.util.JsonUtil;
+import com.lxmajs.demo.util.RedisOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +15,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private RedisOperator redisOperator;
+
     /**
      * 微信用户登录
      * @return
@@ -24,7 +32,15 @@ public class UserController {
 
         String url = "https://api.weixin.qq.com/sns/jscode2session";
 
+        // 发起远程请求微信付武器的接口，获取登录的sessionkey
+        String wxResult = "";
+        WXSessionModel model = JsonUtil.jsonToPojo(wxResult, WXSessionModel.class);
 
+        // 将session存入redis中
+        String redisKey = "user-redis-session:" + model.getOpenid();
+        String redisValue = model.getSession_key();
+        long redisTimeout = 1000 * 60 * 30;
+        redisOperator.set(redisKey, redisValue, redisTimeout);
 
         return modelMap;
     }
