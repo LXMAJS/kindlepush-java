b5961d39a4f7c381ad6604a22238296178fb226f
lxmajs-mbp <lxmajs@163.com>
2019-02-20 17:40:52 +0800

    java thread demo

---
+++ src/main/java/com/temp/demo1/CurrentThreadDemo.java
@@ -0,0 +1,12 @@
+package com.temp.demo1;
+
+public class CurrentThreadDemo {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main ( String[] args ) {
+        System.out.println ("Current Thread : " + Thread.currentThread ().getName ());
+    }
+}
