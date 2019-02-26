768f8d28f4fd8a2e91df0527e056b6d37e771294
lxmajs-mbp <lxmajs@163.com>
2019-02-26 14:55:21 +0800

    deference between WAIT and SLEEP, switch order of WAIT and SLEEP

--- src/main/java/com/temp/demo1/thread/WaitSleepDemo.java
+++ src/main/java/com/temp/demo1/thread/WaitSleepDemo.java
@@ -11,14 +11,13 @@ public class WaitSleepDemo {
         new Thread ( new Runnable () {
             @Override
             public void run () {
-                System.out.println ("Thread A is waiting to get lock");
+                System.out.println ("Thread B is waiting to get lock");
                 synchronized (lock){
                     try {
-                        System.out.println ("Thread A get lock");
+                        System.out.println ("Thread B get lock");
+                        System.out.println ("Thread B is sleeping 10ms");
                         Thread.sleep ( 20 );
-                        System.out.println ("Thread A do wait method");
-                        lock.wait (200);
-                        System.out.println ("Thread A is done");
+                        System.out.println ("Thread B is done");
                     } catch (InterruptedException e) {
                         e.printStackTrace ();
                     }
@@ -30,21 +29,23 @@ public class WaitSleepDemo {
          * 为了让上面的先执行，此处让主线程睡眠10毫秒
          */
         try {
-            Thread.sleep ( 10 );
+            Thread.sleep ( 100 );
         } catch (InterruptedException e) {
             e.printStackTrace ();
         }
 
+
         new Thread ( new Runnable () {
             @Override
             public void run () {
-                System.out.println ("Thread B is waiting to get lock");
+                System.out.println ("Thread A is waiting to get lock");
                 synchronized (lock){
                     try {
-                        System.out.println ("Thread B get lock");
-                        System.out.println ("Thread B is sleeping 10ms");
+                        System.out.println ("Thread A get lock");
                         Thread.sleep ( 20 );
-                        System.out.println ("Thread B is done");
+                        System.out.println ("Thread A do wait method");
+                        lock.wait (200);
+                        System.out.println ("Thread A is done");
                     } catch (InterruptedException e) {
                         e.printStackTrace ();
                     }
