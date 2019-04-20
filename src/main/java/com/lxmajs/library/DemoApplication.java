a6168b226db1a1e5c2afb725ab9f2d310b2ce317
lxmajs-mbp <lxmajs@163.com>
2019-04-20 23:28:18 +0800

    websocket demo 1. complete simple demo in library 2. use ws:/127.0.0.1:5088/chat/XXX to connect websocket server, XXX mean someone's userid

--- src/main/java/com/lxmajs/library/DemoApplication.java
+++ src/main/java/com/lxmajs/library/DemoApplication.java
@@ -3,6 +3,8 @@ package com.lxmajs.library;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 @MapperScan("com.lxmajs.library.dao")
@@ -10,5 +12,16 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/**
+	 * 会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+	 * 要注意，如果使用独立的servlet容器，
+	 * 而不是直接使用springboot的内置容器，
+	 * 就不要注入ServerEndpointExporter，因为它将由容器自己提供和管理。
+	 */
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
 	}
 }
--- src/main/java/com/lxmajs/library/websocket/WebsocketController.java
+++ src/main/java/com/lxmajs/library/websocket/WebsocketController.java
@@ -3,29 +3,75 @@ package com.lxmajs.library.websocket;
 import com.lxmajs.library.util.WebsocketUtil;
 import org.springframework.stereotype.Component;
 
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 /**
  * websocket接口处理类
  */
 @Component
-@ServerEndpoint ( value = "/chat/{usernick}" )
+@ServerEndpoint ( value = "/chat/{userid}" )
 public class WebsocketController {
 
     /**
      * 连接事件，加入注解
-     * @param userNick
+     * @param userId
      * @param session
      */
     @OnOpen
-    public void OnOpen( @PathParam  ( value = "usernick" ) String userNick, Session session ) {
-        String message ="有新游客[" + userNick + "]加入聊天室!";
+    public void onOpen( @PathParam  ( value = "userid" ) String userId, Session session ) {
+        String message ="[" + userId + "]加入聊天室！！";
 
         // 添加到session的映射关系中
-        WebsocketUtil.addSession ( userNick, session );
+        WebsocketUtil.addSession ( userId, session );
+        // 广播通知，某用户上线了
         WebsocketUtil.sendMessageForAll ( message );
     }
+
+    /**
+     * 连接事件，加入注解
+     * 用户断开链接
+     * @param userId
+     * @param session
+     */
+    @OnClose
+    public void onClose(@PathParam  ( value = "userId" ) String userId, Session session ) {
+        String message ="[" + userId + "]退出了聊天室...";
+
+        // 删除映射关系
+        WebsocketUtil.removeSession ( userId );
+        // 广播通知，用户下线了
+        WebsocketUtil.sendMessageForAll ( message );
+    }
+
+    /**
+     * 当接收到用户上传的消息
+     * @param userId
+     * @param session
+     */
+    @OnMessage
+    public void onMessage(@PathParam  ( value = "userId" ) String userId, Session session ,String message) {
+        String msg ="[" + userId + "]:"+message;
+
+        // 直接广播
+        WebsocketUtil.sendMessageForAll ( msg );
+    }
+
+    /**
+     * 处理用户活连接异常
+     * @param session
+     * @param throwable
+     */
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        try {
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        throwable.printStackTrace();
+    }
+
 }
